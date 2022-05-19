03258f2e30c56c40fe5802a2dfa2bf90af218f21
paperhsiao <destiny5420@gmail.com>
2022-05-19 08:56:35 +0800

    feat: 新增 login UI

--- components/Login/index.js
+++ components/Login/index.js
@@ -2,6 +2,21 @@ export default {
   name: 'cLogin',
   data() {
     return {
+      eye: {
+        imgUrl: require(`~/assets/images/login/pic-eyes-01.png`),
+        imgUrl_m: require(`~/assets/images/login/pic-eyes-01.png`),
+        alt: `pic-eyes-01`,
+      },
+      textLogin: {
+        imgUrl: require(`~/assets/images/login/pic-text-01.png`),
+        imgUrl_m: require(`~/assets/images/login/pic-text-01.png`),
+        alt: `pic-text-01`,
+      },
+      textLogin2: {
+        imgUrl: require(`~/assets/images/login/pic-text-02.png`),
+        imgUrl_m: require(`~/assets/images/login/pic-text-02.png`),
+        alt: `pic-text-02`,
+      },
       open: false,
       name: ``,
       email: ``,
